4f02377940e04e39b198f3c479caf7e2809af22e
Alex Bernatowicz <bernatowicz1122@gmail.com>
2023-10-25 10:34:10 -0500

    Welcome Scene logic complete, working on setBetScene 10:33 10/25/23

--- src/main/java/BaccaratGame.java
+++ src/main/java/BaccaratGame.java
@@ -1,5 +1,4 @@
 import javafx.application.Application;
-import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -7,175 +6,158 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.geometry.Insets;
+import java.util.ArrayList;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
-import javafx.scene.text.Text;
-import java.util.ArrayList;
 import java.util.*;
-
 public class BaccaratGame extends Application {
 
-	// Private member variables for UI
-	private Stage primaryStage;
-	private VBox root;
+	// Public member variables
+	private ArrayList<Card> playerHand;
+	private ArrayList<Card> bankerHand;
+	private BaccaratDealer theDealer;
+	private BaccaratGameLogic gameLogic;
+	private double currentBet;
+	private double totalWinnings;
 
-	// Public required member variables
-	public ArrayList<Card> playerHand;
-	public ArrayList<Card> bankerHand;
-	public BaccaratDealer theDealer;
-	public BaccaratGameLogic gameLogic;
-	public double currentBet;
-	public double totalWinnings;
-
-	// TODO: Required method to implement
-	// This method will determine if the user won or lost their bet and return the amount won or
-	// lost based on the value in currentBet.
+	// TODO: FINISH THIS METHOD
+	// Public method
 	public double evaluateWinnings() {
 		double winnings = 0;
-
 		return winnings;
 	}
 
-	// Main method to launch game and all arguments
 	public static void main(String[] args) {
 		launch(args);
 	}
 
+	// Start method that sets title in the beginning and then sets the scene by calling setWelcomeScene
+	// and passing in the primaryStage
 	@Override
 	public void start(Stage primaryStage) {
-		// Creates a stage and sets the title
-		this.primaryStage = primaryStage;
 		primaryStage.setTitle("Baccarat");
 
-		// Sets the stage to the welcome scene using welcomeScene method
-		primaryStage.setScene(welcomeScene());
+		primaryStage.setScene(setWelcomeScene(primaryStage));
 		primaryStage.show();
 	}
 
-	// This method sets the scene for the welcome scene
-	private Scene welcomeScene() {
-		Scene welcomeScene = new Scene(root, 700, 700);
-
-//		// Create a label for the title
-//		Label title = createTitleLabel("Baccarat");
-//
-//		// Create a button for "Click to play!"
-//		Button playButton = createPlayButton();
-//
-//		// Create a GridPane to center the elements
-//		GridPane grid = createCenteredGrid(title, playButton);
-//
-//		// Create the main layout VBox for the initial scene
-//		VBox initialRoot = createCenteredLayout(grid);
-
-
-        return welcomeScene;
-    }
-
-	private Scene setBetScene() {
-		primaryStage.setTitle("Baccarat");
-		Scene betScene = new Scene(root, 700, 700);;
-
-		return betScene;
-	}
-
-	// Method to set up scene to place bets (Scene 2)
-//	private Scene setBetScene() {
-//		Label title = createTitleLabel("Baccarat");
-//
-//		Button playerButton = new Button("PLAYER");
-//		Button bankerButton = new Button("BANKER");
-//		Button drawButton = new Button("DRAW");
-//
-//		VBox buttonBox = createCenteredLayout(title, playerButton, bankerButton, drawButton);
-//
-//		Label selectBetLabel = new Label("Select a bet amount:");
-//
-//		HBox circleButtonBox = createCircleButtonBox();
-//
-//		Label totalBetLabel = new Label("Total Bet: ");
-//
-//		HBox rectangularButtonBox = createRectangularButtonBox();
-//
-//		Label winningsLabel = new Label("Winnings: ");
-//
-//		VBox newRoot = createCenteredLayout(buttonBox, selectBetLabel, circleButtonBox, totalBetLabel, rectangularButtonBox, winningsLabel);
-//
-//		Scene newScene = createScene(newRoot);
-//	}
-
-	private Label createTitleLabel(String text) {
-		Label title = new Label(text);
+	// This method sets the scene to welcome the player and lets them start playing the game
+	private Scene setWelcomeScene(Stage primaryStage) {
+		// Create a label for the title
+		Label title = new Label("Baccarat");
 		title.setTextFill(Color.RED);
-		title.setStyle("-fx-font-size: 36px");
-		return title;
-	}
+		title.setStyle("-fx-font-size: 36px;"); // Increase the font size
 
-	private Button createPlayButton() {
+		// Create a button
 		Button playButton = new Button("Click to play!");
-		playButton.setOnAction(event -> setBetScene());
-		playButton.setAlignment(Pos.CENTER);
-		VBox.setMargin(playButton, new Insets(20, 0, 0, 0));
-		return playButton;
-	}
+		playButton.setOnAction(e -> {
 
-	private GridPane createCenteredGrid(Label title, Button playButton) {
+			// Transition to the bet scene when the button is clicked
+			primaryStage.setScene(setBetScene(primaryStage));
+		});
+
+		// Center the button text
+		playButton.setAlignment(Pos.CENTER);
+
+		// Add padding between the title and the button
+		VBox.setMargin(playButton, new Insets(20, 0, 0, 0));
+
+		// Create a GridPane to center the elements
 		GridPane grid = new GridPane();
 		grid.setAlignment(Pos.CENTER);
+
+		// Add the title and button to the grid
 		grid.add(title, 0, 0);
 		grid.add(playButton, 0, 1);
-		return grid;
-	}
 
-	private VBox createCenteredLayout(GridPane content) {
-		VBox root = new VBox(content);
+		// Create the main layout VBox
+		VBox root = new VBox(grid);
 		root.setAlignment(Pos.CENTER);
-		return root;
+
+		// Create the scene
+		Scene scene = new Scene(root, 700, 700);
+
+		return scene;
 	}
 
-	// This method creates a new scene that is just a template
-//	private Scene createScene() {
-//		Scene scene = new Scene(root, 700, 700);
-//		primaryStage.setTitle("Baccarat");
-//		return scene;
-//	}
+	// This method sets the scene for a player to place their bets
+	private Scene setBetScene(Stage primaryStage) {
+		primaryStage.setTitle("Baccarat");
 
-	private HBox createCircleButtonBox() {
-		Circle dollar1Button = createCircleButton("$1", Color.GREEN);
-		Circle dollar5Button = createCircleButton("$5", Color.BLUE);
-		Circle dollar10Button = createCircleButton("$10", Color.YELLOW);
-		Circle dollar25Button = createCircleButton("$25", Color.ORANGE);
-		Circle dollar50Button = createCircleButton("$50", Color.RED);
+		// Create a label for the title
+		Label title = new Label("Baccarat");
+		title.setTextFill(Color.RED);
+		title.setStyle("-fx-font-size: 36px;"); // Increase the font size
 
+		// Create buttons for "PLAYER," "BANKER," and "DRAW"
+		Button playerButton = new Button("PLAYER");
+		Button bankerButton = new Button("BANKER");
+		Button drawButton = new Button("DRAW");
+
+		// Create a VBox to stack the buttons vertically
+		VBox buttonBox = new VBox(title, playerButton, bankerButton, drawButton);
+		buttonBox.setAlignment(Pos.CENTER);
+
+		// Create a label for "Select a bet amount"
+		Label selectBetLabel = new Label("Select a bet amount:");
+
+		// Create circle buttons for "$1", "$5", "$10", "$25", and "$50"
+		Circle dollar1Button = new Circle(25, Color.GREEN);
+		dollar1Button.setStroke(Color.BLACK);
+		dollar1Button.setStrokeWidth(2);
+		Circle dollar5Button = new Circle(25, Color.BLUE);
+		dollar5Button.setStroke(Color.BLACK);
+		dollar5Button.setStrokeWidth(2);
+		Circle dollar10Button = new Circle(25, Color.YELLOW);
+		dollar10Button.setStroke(Color.BLACK);
+		dollar10Button.setStrokeWidth(2);
+		Circle dollar25Button = new Circle(25, Color.ORANGE);
+		dollar25Button.setStroke(Color.BLACK);
+		dollar25Button.setStrokeWidth(2);
+		Circle dollar50Button = new Circle(25, Color.RED);
+		dollar50Button.setStroke(Color.BLACK);
+		dollar50Button.setStrokeWidth(2);
+
+		// Create an HBox for the circle buttons
 		HBox circleButtonBox = new HBox(dollar1Button, dollar5Button, dollar10Button, dollar25Button, dollar50Button);
 		circleButtonBox.setAlignment(Pos.CENTER);
 		circleButtonBox.setSpacing(10);
 
-		return circleButtonBox;
-	}
+		// Create a label for "Total Bet"
+		Label totalBetLabel = new Label("Total Bet: ");
 
-	private Circle createCircleButton(String text, Color color) {
-		Circle button = new Circle(25, color);
-		button.setStroke(Color.BLACK);
-		button.setStrokeWidth(2);
-		return button;
-	}
+		// Create rectangular buttons for "Clear Bets" and "Draw"
+		Rectangle clearBetsButton = new Rectangle(80, 30, Color.LIGHTGRAY);
+		clearBetsButton.setStroke(Color.BLACK);
+		clearBetsButton.setStrokeWidth(2);
+		Rectangle drawButton2 = new Rectangle(80, 30, Color.LIGHTGRAY);
+		drawButton2.setStroke(Color.BLACK);
+		drawButton2.setStrokeWidth(2);
 
-	private HBox createRectangularButtonBox() {
-		Rectangle clearBetsButton = createRectangularButton("Clear Bets");
-		Rectangle drawButton = createRectangularButton("Draw");
-
-		HBox rectangularButtonBox = new HBox(clearBetsButton, drawButton);
+		// Create an HBox for the rectangular buttons
+		HBox rectangularButtonBox = new HBox(clearBetsButton, drawButton2);
 		rectangularButtonBox.setAlignment(Pos.CENTER);
 		rectangularButtonBox.setSpacing(10);
 
-		return rectangularButtonBox;
+		// Create a label for "Winnings"
+		Label winningsLabel = new Label("Winnings: ");
+
+		// Create a VBox to stack the elements
+		VBox root = new VBox(
+				buttonBox,
+				selectBetLabel,
+				circleButtonBox,
+				totalBetLabel,
+				rectangularButtonBox,
+				winningsLabel
+		);
+		root.setAlignment(Pos.CENTER);
+		root.setSpacing(20);
+
+		// Create the scene
+		Scene scene = new Scene(root, 700, 700);
+		return scene;
 	}
 
-	private Rectangle createRectangularButton(String text) {
-		Rectangle button = new Rectangle(80, 30, Color.LIGHTGRAY);
-		button.setStroke(Color.BLACK);
-		button.setStrokeWidth(2);
-		return button;
-	}
 }
